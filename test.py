14d3248b2346ad1f03c9eb02bfb71515ee20f6e5
BAE BYEONGIL <b000202@naver.com>
2024-02-16 23:06:59 +0900

    Romove unnecessary error messages

--- test.py
+++ test.py
@@ -2,10 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
+# 크롬 드라이버 자동 업데이트
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 chrome_option = Options()
+
+# 브라우저 꺼짐 방지
 chrome_option.add_experimental_option("detach", True)
+# 불필요한 에러 메시지 없애기
+chrome_option.add_experimental_option("excludeSwitches", ["enable-logging"])
 
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_option)
